caacde13b667b9df0a9a60f18f38ce70390a7a5c
Kamil A. Kaczmarek <kamil.kaczmarek@neptune.ai>
2021-06-10 12:14:08 +0200

    Kk/docs (#3)
    * fixed small trees visuals problem, by controlling figure size
    
    * added docstrings to NeptuneCallback, formatting from <'> to <">
    
    * dropped unnecessary indent
    
    * Added booster summary docstrings
    
    * corrections.
    
    * correction

--- neptune_lightgbm/impl/__init__.py
+++ neptune_lightgbm/impl/__init__.py
@@ -15,8 +15,8 @@
 #
 
 __all__ = [
-    'create_booster_summary',
-    'NeptuneCallback',
+    "create_booster_summary",
+    "NeptuneCallback",
 ]
 
 import subprocess
@@ -41,64 +41,102 @@ except ImportError:
     import neptune
     from neptune.internal.utils import verify_type
 
-INTEGRATION_VERSION_KEY = 'source_code/integrations/neptune-lightgbm'
+INTEGRATION_VERSION_KEY = "source_code/integrations/neptune-lightgbm"
 
 
 class NeptuneCallback:
-    """Callable class meant for logging lightGBM learning curves to Neptune.
+    """Neptune callback for logging metadata during LightGBM model training.
 
-    Goes over the list of metrics and valid_sets passed to the `lgb.train`
-    object and logs them to a separate channels. For example with 'objective': 'multiclass'
-    and `valid_names=['train','valid']` there will be 2 channels created:
-    `train_multiclass_logloss` and `valid_multiclass_logloss`.
-    Object of this class should be passed to the `callbacks` parameter of the `lgb.train` function.
+    See guide with examples in the `Neptune-LightGBM docs`_.
+
+    This callback logs parameters, evaluation results and info about the train_set:
+    feature names, number of datapoints (``num_rows``) and number of features (``num_features``).
+
+    Evaluation results are logged separately for every ``valid_sets``.
+    For example with `"metric": "logloss"` and `valid_names=["train","valid"]`, 2 logs are created:
+    `train/logloss` and `valid/logloss`.
+
+    Callback works with ``lgbm.train()`` and ``lgbm.cv()`` functions, and with the sklearn API ``model.fit()``.
+    For more details see `Neptune-LightGBM docs`_.
+
+    Note:
+        You can use public ``api_token="ANONYMOUS"`` and set ``project="common/lightgbm-integration"``
+        for testing without registration.
 
     Args:
-        run(`neptune.new.run.Run`): Neptune Run.
-            If this parameter is skipped then the last created Neptune Run in this process will be used.
-        base_namespace(str): Prefix that should be added before the `metric_name`
-            and `valid_name` before logging to the appropriate channel.
+        run (:obj:`neptune.new.run.Run`): Neptune run object.
+            A run in Neptune is a representation of all metadata that you log to Neptune.
+            Learn more in `run docs`_.
+        base_namespace(:obj:`str`, optional): Root namespace. All metadata will be logged inside.
+            Default is empty string. In this case metadata is logged without common "base_namespace".
 
     Examples:
-        Prepare dataset::
+        For more examples visit `example scripts`_.
+
+        Full script that does model training and logging of the metadata::
+
             import lightgbm as lgb
+            import neptune.new as neptune
+            from neptune.new.integrations.lightgbm import NeptuneCallback
+            from sklearn.datasets import load_digits
             from sklearn.model_selection import train_test_split
-            from sklearn.datasets import load_wine
-            data = load_wine()
-            X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.1)
+
+            # Create run
+            run = neptune.init(
+                project="common/lightgbm-integration",
+                api_token="ANONYMOUS",
+                name="train-cls",
+                tags=["lgbm-integration", "train", "cls"]
+            )
+
+            # Create neptune callback
+            neptune_callback = NeptuneCallback(run=run)
+
+            # Prepare data
+            X, y = load_digits(return_X_y=True)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
             lgb_train = lgb.Dataset(X_train, y_train)
             lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
-        Define model parameters::
-            params = {'boosting_type': 'gbdt',
-                      'objective': 'multiclass',
-                      'num_class': 3,
-                      'num_leaves': 31,
-                      'learning_rate': 0.05,
-                      'feature_fraction': 0.9
-                      }
-        Initialize Neptune Run::
-            run = neptune.init()
-        Run `lgb.train` passing `NeptuneCallback` to the `callbacks` parameter::
-            gbm = lgb.train(params,
-                            lgb_train,
-                            num_boost_round=500,
-                            valid_sets=[lgb_train, lgb_eval],
-                            valid_names=['train','valid'],
-                            callbacks=[NeptuneCallback(run)],
-                           )
-    Note:
-        If you are running a k-fold validation it is a good idea to add the k-fold prefix
-        and pass it to the `NeptuneCallback` constructor::zz
-            prefix='fold_{}'.format(fold_id)
-            monitor = NeptuneCallback(base_namespace=prefix)
+
+            # Define parameters
+            params = {
+                "boosting_type": "gbdt",
+                "objective": "multiclass",
+                "num_class": 10,
+                "metric": ["multi_logloss", "multi_error"],
+                "num_leaves": 21,
+                "learning_rate": 0.05,
+                "feature_fraction": 0.9,
+                "bagging_fraction": 0.8,
+                "bagging_freq": 5,
+                "max_depth": 12,
+            }
+
+            # Train the model and log metadata to the run in Neptune
+            gbm = lgb.train(
+                params,
+                lgb_train,
+                num_boost_round=200,
+                valid_sets=[lgb_train, lgb_eval],
+                valid_names=["training", "validation"],
+                callbacks=[neptune_callback],
+            )
+
+    .. _Neptune-LightGBM docs:
+        https://docs.neptune.ai/integrations-and-supported-tools/model-training/lightgbm
+       _run docs:
+        https://docs.neptune.ai/api-reference/run
+       _example scripts:
+        https://github.com/neptune-ai/examples/tree/main/integrations-and-supported-tools/lightgbm/scripts
+
     """
 
-    def __init__(self, run: 'neptune.Run', base_namespace=''):
-        verify_type('run', run, neptune.Run)
-        verify_type('base_namespace', base_namespace, str)
+    def __init__(self, run: 'neptune.Run', base_namespace=""):
+        verify_type("run", run, neptune.Run)
+        verify_type("base_namespace", base_namespace, str)
 
-        if base_namespace and not base_namespace.endswith('/'):
-            base_namespace += '/'
+        if base_namespace and not base_namespace.endswith("/"):
+            base_namespace += "/"
 
         self._run = run
         self._base_namespace = base_namespace
@@ -148,17 +186,145 @@ class NeptuneCallback:
 
 
 def create_booster_summary(
-        booster: Union[lgb.Booster, lgb.sklearn.LGBMModel],
-        log_importances: bool = True,
-        max_num_features: int = 10,
-        list_trees: list = None,
-        log_trees_as_dataframe: bool = True,  # works only for lgb.Booster
-        log_pickled_booster: bool = True,
-        log_trees: bool = False,  # requires graphviz
-        log_confusion_matrix: bool = False,  # requires scikit-plot
-        y_true: np.ndarray = None,
-        y_pred: np.ndarray = None,
+    booster: Union[lgb.Booster, lgb.sklearn.LGBMModel],
+    log_importances: bool = True,
+    max_num_features: int = 10,
+    list_trees: list = None,
+    log_trees_as_dataframe: bool = True,
+    log_pickled_booster: bool = True,
+    log_trees: bool = False,
+    tree_figsize: int = 30,
+    log_confusion_matrix: bool = False,
+    y_true: np.ndarray = None,
+    y_pred: np.ndarray = None,
 ):
+    """Create model summary after training that can be assigned to the run namespace.
+
+    See guide with examples in the `Neptune-LightGBM docs`_.
+
+    You can log multiple types of metadata:
+        - pickled model
+        - feature importance chart
+        - visualized trees
+        - trees represented as DataFrame
+        - confusion matrix (only for classification problems)
+
+    See Args section for more info how to parametrize behaviour of this function.
+
+    Note:
+        You can log summary to the new run, or to the same run that you used for logging model training.
+        Second option can be very useful because you have all the information in the single run.
+
+    Args:
+        booster (:obj:`lgb.Booster` or :obj:`lgb.sklearn.LGBMModel`): Trained LightGBM model.
+        log_importances (bool): Defaults to True. Log feature importance charts.
+        max_num_features (int): Defaults to 10. Max number of top features on the importance charts.
+            Works only if ``log_importances`` is set to ``True``.
+            If None or <1, all features will be displayed.
+            See `lightgbm.plot_importance`_ for details.
+        list_trees (list): Defaults to None. Indices of the target tree to visualize.
+            Works only if ``log_trees`` is set to ``True``.
+            See `lightgbm.plot_tree`_ for details.
+        log_trees_as_dataframe (bool): Defaults to True.
+            Parse the model and log trees in the easy-to-read pandas DataFrame format.
+            Works only for ``lgb.Booster``.
+            See `lightgbm.Booster.trees_to_dataframe`_ for details.
+        log_pickled_booster (bool): Defaults to True. Log model as pickled file.
+        log_trees (bool): Defaults to False. Log visualized trees.
+            This requires graphviz to work. Learn about setup in the `Neptune-LightGBM installation`_ docs.
+        tree_figsize (int): Defaults to 30, Control size of the visualized tree image.
+            Increase this in case you work with large trees.
+            Works only if ``log_trees`` is set to ``True``.
+        log_confusion_matrix (bool): Defaults to False. Log confusion matrix.
+            If set to True, you need to pass ``y_true`` and ``y_pred``.
+        y_true (:obj:`np.ndarray`): Defaults to None. True labels on the test set.
+            Needed only if ``log_confusion_matrix`` is set to True.
+        y_pred (:obj:`np.ndarray`): Defaults to None. Predictions on the test set.
+            Needed only if ``log_confusion_matrix`` is set to True.
+
+    Returns:
+        dict: Python dictionary with all metadata, that can be assigned to the run namespace.
+            ``run["booster_summary"] = create_booster_summary(...)``
+
+    Examples:
+        For more examples visit `example scripts`_.
+
+        Full script that does logging during model training and logs booster summary after training::
+
+            import lightgbm as lgb
+            import neptune.new as neptune
+            import numpy as np
+            from neptune.new.integrations.lightgbm import NeptuneCallback, create_booster_summary
+            from sklearn.datasets import load_digits
+            from sklearn.model_selection import train_test_split
+
+            # Create run
+            run = neptune.init(
+                project="common/lightgbm-integration",
+                api_token="ANONYMOUS",
+                name="train-cls",
+                tags=["lgbm-integration", "train", "cls"]
+            )
+
+            # Create neptune callback
+            neptune_callback = NeptuneCallback(run=run)
+
+            # Prepare data
+            X, y = load_digits(return_X_y=True)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+            lgb_train = lgb.Dataset(X_train, y_train)
+            lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
+
+            # Define parameters
+            params = {
+                "boosting_type": "gbdt",
+                "objective": "multiclass",
+                "num_class": 10,
+                "metric": ["multi_logloss", "multi_error"],
+                "num_leaves": 21,
+                "learning_rate": 0.05,
+                "feature_fraction": 0.9,
+                "bagging_fraction": 0.8,
+                "bagging_freq": 5,
+                "max_depth": 12,
+            }
+
+            # Train the model and log metadata to the run in Neptune
+            gbm = lgb.train(
+                params,
+                lgb_train,
+                num_boost_round=200,
+                valid_sets=[lgb_train, lgb_eval],
+                valid_names=["training", "validation"],
+                callbacks=[neptune_callback],
+            )
+
+            y_pred = np.argmax(gbm.predict(X_test), axis=1)
+
+            # Log summary metadata to the same run under the "lgbm_summary" namespace
+            run["lgbm_summary"] = create_booster_summary(
+                booster=gbm,
+                log_trees=True,
+                list_trees=[0, 1, 2, 3, 4],
+                log_confusion_matrix=True,
+                y_pred=y_pred,
+                y_true=y_test
+            )
+
+    .. _Neptune-LightGBM docs:
+        https://docs.neptune.ai/integrations-and-supported-tools/model-training/lightgbm
+       _lightgbm.plot_importance:
+        https://lightgbm.readthedocs.io/en/latest/pythonapi/lightgbm.plot_importance.html#lightgbm-plot-importance
+       _lightgbm.plot_tree:
+        https://lightgbm.readthedocs.io/en/latest/pythonapi/lightgbm.plot_tree.html#lightgbm-plot-tree
+       _lightgbm.Booster.trees_to_dataframe:
+        https://lightgbm.readthedocs.io/en/latest/pythonapi/lightgbm.Booster.html#lightgbm.Booster.trees_to_dataframe
+       _Neptune-LightGBM installation:
+        https://docs.neptune.ai/integrations-and-supported-tools/model-training/lightgbm#install-requirements
+       _example scripts:
+        https://github.com/neptune-ai/examples/tree/main/integrations-and-supported-tools/lightgbm/scripts
+
+    """
     results_dict = {}
     visuals_path = "visualizations/"
     if log_importances:
@@ -181,7 +347,7 @@ def create_booster_summary(
 
     if log_trees:
         try:
-            subprocess.call(['dot', '-V'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.call(["dot", "-V"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         except OSError:
             log_trees = False
             message = "Graphviz executables not found, so trees will not be logged. " \
@@ -191,13 +357,13 @@ def create_booster_summary(
     if log_trees:
         trees_series = []
         for i in list_trees:
-            digraph = lgb.create_tree_digraph(booster, tree_index=i, show_info='data_percentage')
-            _, ax = plt.subplots(1, 1)
+            digraph = lgb.create_tree_digraph(booster, tree_index=i, show_info="data_percentage")
+            _, ax = plt.subplots(1, 1, figsize=(tree_figsize, tree_figsize))
             s = BytesIO()
-            s.write(digraph.pipe(format='png'))
+            s.write(digraph.pipe(format="png"))
             s.seek(0)
             ax.imshow(image.imread(s))
-            ax.axis('off')
+            ax.axis("off")
             trees_series.append(neptune.types.File.as_image(ax.figure))
         results_dict["{}trees".format(visuals_path)] = neptune.types.FileSeries(trees_series)
 
